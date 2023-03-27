41f820b742d964d8c55fa3527359501e4b290bbf
Gizaw Dagne <gizawaait@gmail.com>
2023-03-27 21:23:31 +0300

    I added test for to_lower() and capitalize() functions and correct some bugs in the string_functions.py file

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import unittest
 from string_functions import *
 
 class TestToUpper(unittest.TestCase):
-    # Test the to_upper functions
+
     def test_to_upper(self):
         # Assert with all capital letters string
         # Input: "PYTHON"
@@ -34,12 +34,53 @@ class TestToUpper(unittest.TestCase):
         # Output: TypeError
         self.assertRaises(TypeError, to_upper, 123)
 class TestToLower(unittest.TestCase):
-    # Write you code here
-    pass
+    # Test the to_lower functions.
+
+    def test_to_lower(self):
+        # Assert with all capital letters string
+        # Input: "HELLO"
+        # Output: "hello"
+        self.assertEqual(to_lower("HELLO"), "hello")
+
+        # Assert with a mixed lower and upper letters.
+        # Input: "HeLlO"
+        # Output: "hello"
+        self.assertEqual(to_lower("HeLlO"), "hello")
+        
+        # Assert with a numerical digits.
+        # Input: "HeLlO"
+        # Output: "hello"
+        self.assertEqual(to_lower("12B3"), "12b3")
+
+        # Assert empty string.
+        # Input: "HeLlO"
+        # Output: "hello"
+        self.assertEqual(to_lower(""), "")
 
 class TestCapitalize(unittest.TestCase):
-    # Write your code here
-    pass
+    # Test the capitalize functions.
 
+    def test_capitalize(self):
+
+        # Assert with all small letters string
+        # Input: "hello"
+        # Output: "Hello"
+        self.assertEqual(capitalize("hello"), "Hello")
+
+        # Assert a string with a mix of small and capital letters.
+        # Input: "HeLlO"
+        # Output: "Hello"
+        self.assertEqual(capitalize("HeLlO"), "Hello")
+        
+        # Assert with a numerical digits.
+        # Input: "HeLlO"
+        # Output: "hello"
+        self.assertEqual(capitalize("12B3"), "12b3")
+
+        # Assert empty string.
+        # Input: "HeLlO"
+        # Output: "hello"
+        self.assertEqual(capitalize(""), "")
+        
 if __name__ == '__main__':
     unittest.main()
