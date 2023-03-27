21176a667deab924b8952d59f9db513e7f5b94e1
Biruk Tassew <biruktassew21@gmail.com>
2023-03-27 08:04:29 +0300

    added a test for to_upper function

--- test.py
+++ test.py
@@ -2,9 +2,37 @@ import unittest
 from string_functions import *
 
 class TestToUpper(unittest.TestCase):
-    # write your code here
-    pass
+    # Test the to_upper functions
+    def test_to_upper(self):
+        # Assert with all capital letters string
+        # Input: "PYTHON"
+        # Output: "PYTHON"
+        self.assertEqual(to_upper("PYTHON"), "PYTHON")
 
+        # Assert with all small letters string
+        # Input: "python"
+        # Output: "PYTHON"
+        self.assertEqual(to_upper("python"), "PYTHON")
+
+        # Assert with all jumbled letters
+        # Input: "pYtHoN"
+        # Output: "PYTHON"
+        self.assertEqual(to_upper("pYtHoN"), "PYTHON")
+
+        # Assert with empty string
+        # Input: ""
+        # Output: ""
+        self.assertEqual(to_upper(""), "")
+
+        # Assert with a string integer
+        # Input: "123"
+        # Output: "123"
+        self.assertEqual(to_upper("123"), "123")
+
+        # Assert with an integer
+        # Input: 123
+        # Output: TypeError
+        self.assertRaises(TypeError, to_upper, 123)
 class TestToLower(unittest.TestCase):
     # Write you code here
     pass
